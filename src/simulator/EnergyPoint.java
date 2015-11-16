983ebb2e4973d5dcb34d9ef501e3e2540f3a0dea
Daniel Arasu ANBARASU3 <anbarasu@polytech.unice.fr>
2015-11-16 15:43:53 +0100

    Energy points fonctionnent

---
+++ src/simulator/EnergyPoint.java
@@ -0,0 +1,69 @@
+package simulator;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics2D;
+import java.awt.geom.*;
+
+import creatures.IEnvironment;
+import visual.*;
+
+public class EnergyPoint implements IDrawable {
+	
+	protected final Point2D position;
+	
+	protected final IEnvironment environment;
+	
+	protected static final int size = 50;
+	
+	private static final Color color = new Color(255,165,0,128);
+	
+	public EnergyPoint(IEnvironment environment, Point2D position) {
+		this.environment = environment;
+		
+		double x = position.getX();
+		double y = position.getY();
+		Dimension s = environment.getSize();
+		
+		if (x > s.getWidth() / 2) {
+			x = -s.getWidth() / 2;
+		} else if (x < -s.getWidth() / 2) {
+			x = s.getWidth() / 2;
+		}
+
+		if (y > s.getHeight() / 2) {
+			y = -s.getHeight() / 2;
+		} else if (y < -s.getHeight() / 2) {
+			y = s.getHeight() / 2;
+		}
+
+		this.position = new Point2D.Double(x, y);
+	}
+	
+	@Override
+	public void paint(Graphics2D g2) {
+		g2.translate(position.getX(), position.getY());
+		g2.translate(-size / 2, -size / 2);
+		g2.setColor(color);
+		g2.fillOval((int)position.getX(), (int)position.getY(), size, size);
+	}
+	
+	@Override
+	public Color getColor() {
+		return color;
+	}
+	
+	@Override
+	public int getSize() {
+		return size;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "EnergyPoint";
+	}
+	
+
+
+}
---
+++ src/visual/Visualizer.java
@@ -0,0 +1,50 @@
+package visual;
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.AffineTransform;
+
+import javax.swing.JPanel;
+
+import com.sun.media.jfxmedia.logging.Logger;
+
+@SuppressWarnings("serial")
+public abstract class Visualizer extends JPanel {
+
+	@Override
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		
+		Graphics2D g2 = (Graphics2D) g;
+		
+		// save transformation
+		AffineTransform pT = g2.getTransform();
+		g2.transform(getTransform());
+//		g2.translate(getWidth() / 2, getHeight() / 2);
+
+		paint(g2);
+
+		// restore transformation
+		g2.setTransform(pT);
+	}
+
+	protected void paint(Graphics2D g2) {
+		// draw all creatures
+		for (IDrawable d : getDrawables()) {
+			// save transformation for each drawable
+			AffineTransform cT = g2.getTransform();
+			System.out.println(d);
+			d.paint(g2);
+
+			// restore transformation
+			g2.setTransform(cT);
+		}
+	}
+
+	protected AffineTransform getTransform() {
+		return AffineTransform.getTranslateInstance(getWidth() / 2.0, getHeight() / 2.0);
+	}
+	
+	protected abstract Iterable<? extends IDrawable> getDrawables();
+
+}
